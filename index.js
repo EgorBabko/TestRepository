ff2e5a4d7719af743c0a54f2998dfdf10b860c95
unknown <36426383@mail.ru>
2022-02-06 12:39:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const clients = [{
+        id: 1,
+        name: 'Аристарх',
+        lastName: 'Петров'
+    },
+    {
+        id: 2,
+        name: 'Сергей',
+        lastName: 'Миронов'
+    },
+    {
+        id: 3,
+        name: 'Игнатий',
+        lastName: 'Семашко'
+    },
+    {
+        id: 4,
+        name: 'Константин',
+        lastName: 'Илларионов'
+    },
+    {
+        id: 5,
+        name: 'Ольга',
+        lastName: 'Меньшова'
+    },
+    {
+        id: 6,
+        name: 'Вероника',
+        lastName: 'Гиль'
+    },
+    {
+        id: 7,
+        name: 'Борис',
+        lastName: 'Поганкин'
+    },
+    {
+        id: 8,
+        name: 'Вероника',
+        lastName: 'Чижова'
+    },
+    {
+        id: 9,
+        name: 'Мария',
+        lastName: 'Кулешова'
+    },
+    {
+        id: 10,
+        name: 'Валерий',
+        lastName: 'Красный'
+    },
+    {
+        id: 11,
+        name: 'Ипполит',
+        lastName: 'Тесеев'
+    },
+    {
+        id: 12,
+        name: 'Данил',
+        lastName: 'Киров'
+    },
+    {
+        id: 13,
+        name: 'Евгений',
+        lastName: 'Потапенко'
+    },
+    {
+        id: 14,
+        name: 'Дмитрий',
+        lastName: 'Кузмич'
+    },
+    {
+        id: 15,
+        name: 'Алексей',
+        lastName: 'Лавров'
+    },
+]
